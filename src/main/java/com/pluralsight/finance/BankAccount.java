539d5fa78083dd7d7bfc5c5983169be639b8efb7
marbin701 <marvin07012002@gmail.com>
2026-05-18 21:17:23 -0400

    BankAccount class finished.

---
+++ src/main/java/com/pluralsight/finance/BankAccount.java
@@ -0,0 +1,25 @@
+package com.pluralsight.finance;
+
+public class BankAccount {
+    private String name;
+    private String accountNumber;
+    private double balance;
+
+    public BankAccount(String name,String accountNumber,double balance){
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount){
+        this.balance += amount;
+    }
+
+    public  void withdraw(double amount){
+        this.balance -= amount;
+    }
+
+    public double getValue(){
+        return this.balance;
+    }
+}
