201d84b181430fe1d8f2704fc6d20a32a7969696
marbin701 <marvin07012002@gmail.com>
2026-05-18 21:26:26 -0400

    working in main class

---
+++ src/main/java/com/pluralsight/finance/Main.java
@@ -0,0 +1,11 @@
+package com.pluralsight.finance;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BankAccount account1 = new BankAccount("Pam","123",12500);
+        Valuable account2 = new BankAccount("Gary","456",1500);
+
+    }
+}
