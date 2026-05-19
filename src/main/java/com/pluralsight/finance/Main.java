64fee9136a10e14bebf402879d2f207fe34cad58
marbin701 <marvin07012002@gmail.com>
2026-05-18 21:40:12 -0400

    working in main class, add instanceof for the main class

--- src/main/java/com/pluralsight/finance/Main.java
+++ src/main/java/com/pluralsight/finance/Main.java
@@ -7,5 +7,14 @@ public class Main {
         BankAccount account1 = new BankAccount("Pam","123",12500);
         Valuable account2 = new BankAccount("Gary","456",1500);
 
+
+        if(account2 instanceof BankAccount){
+            BankAccount bankAccount2 =(BankAccount) account2;
+            ((BankAccount) account2).deposit(100);
+        }
+
+        account1.deposit(100);
+
+
     }
 }
